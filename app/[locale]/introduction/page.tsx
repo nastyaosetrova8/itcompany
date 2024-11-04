117d7d302cfd2d2f8026f2df6eca07ea2baf28d1
nastyaosetrova8 <126395277+nastyaosetrova8@users.noreply.github.com>
2024-11-04 08:45:04 +0100

    add introduction page

--- app/[locale]/introduction/page.tsx
+++ app/[locale]/introduction/page.tsx
@@ -9,14 +9,22 @@ const IntroductionPage: React.FC<Props> = async ({ params: { locale } }) => {
   const t = await getTranslations("Introduction");
 
   return (
-    <section className="bg-neutral-300 ">
-      <div className="container relative flex justify-center items-center bg-[url('/images/containerBGSecondary3.webp')] bg-cover bg-center bg-no-repeat w-full h-screen">
-        <div className="absolute top-[7%] left-[6%] bottom-[18%] xl:max-w-[590px] w-full px-16 pt-11 pb-2  bg-neutral-100 rounded-3xl overflow-y-auto scroll-containerCl">
-          <p className="subTitleCl">{t("introduction") || ""}</p>
-          <h1 className="titleCl ">{t("title") || ""}</h1>
-          <p className="descriptionCl">{t("p1") || ""}</p>
-          <p className="descriptionCl">{t("p2") || ""}</p>
-          <p className="descriptionCl">{t("p3") || ""}</p>
+    <section className="h-screen flex items-center bg-neutral-300">
+      <div className="container relative flex justify-center items-center bg-[url('/images/containerBGSecondary3.webp')] bg-cover bg-center bg-no-repeat h-dynamic w-full">
+        <div className="absolute top-[7%] left-[6%] bottom-[18%] xl:max-w-[590px] w-full bg-neutral-100 rounded-3xl">
+          <div className="w-full h-full px-16 pt-11 pb-40  overflow-y-auto scroll-containerCl">
+            {/* <div className="absolute top-[7%] left-[6%] bottom-[18%] xl:max-w-[590px] w-full px-16 pt-11 pb-40 bg-neutral-100 rounded-3xl overflow-y-auto scroll-containerCl"> */}
+            <p className="subTitleCl">{t("introduction") || ""}</p>
+            <h1 className="titleCl ">{t("title") || ""}</h1>
+            <p className="descriptionCl">{t("p1") || ""}</p>
+            <p className="descriptionCl">{t("p2") || ""}</p>
+            <p className="descriptionCl">{t("p3") || ""}</p>
+          </div>
+          <div className="absolute right-0 bottom-0 rotate-90 -translate-y-[500%] translate-x-[20%]">
+            <p className=" before:content-[''] before:h-[1px] before:w-11 before:mr-3 flex items-center before:bg-stone-950">
+              {t("scroll") || ""}
+            </p>
+          </div>
         </div>
         <div></div>
       </div>
@@ -25,3 +33,6 @@ const IntroductionPage: React.FC<Props> = async ({ params: { locale } }) => {
 };
 
 export default IntroductionPage;
+
+// relative;
+// before:absolute before:t-0.5 before:left-0
